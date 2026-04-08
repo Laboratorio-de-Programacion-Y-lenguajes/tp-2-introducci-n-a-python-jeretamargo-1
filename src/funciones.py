49bfb3d8fb68e30c7dfecacf5f1e743e8619e1d7
jeretamargo <jeretamargo01@gmail.com>
2026-04-07 21:56:11 -0300

    funciones.py realizado

--- src/funciones.py
+++ src/funciones.py
@@ -7,7 +7,10 @@ def aplicar_funcion(lista: list, func) -> list:
     """
     Aplica func a cada elemento de la lista y retorna la nueva lista.
     """
-    # TU CÓDIGO AQUÍ
+    nuevaLista: list = []
+    for el in lista:
+      nuevaLista.append(func(el))  
+    return nuevaLista
     pass
 
 
@@ -16,7 +19,12 @@ def componer(f, g):
     Retorna una nueva función que aplica g y luego f.
     Ejemplo: componer(f, g)(x) == f(g(x))
     """
-    # TU CÓDIGO AQUÍ
+    def componer2(x):
+        x = g(x)
+        x = f(x)
+        return x
+
+    return componer2
     pass
 
 
@@ -26,7 +34,13 @@ def memoizar(func):
     Si se llama con los mismos argumentos, retorna el resultado cacheado.
     """
     cache = {}
-    # TU CÓDIGO AQUÍ
+    def memorizado(*args, **kwargs):
+        # Crear una clave única basada en los argumentos
+        key = (args, tuple(sorted(kwargs.items())))
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return memorizado
     pass
 
 
